dc63aac80e040d2f7a6dc81cde8d70c82c787ffc
cbkinase <cambeck366@gmail.com>
2023-02-16 17:07:55 -0500

    .

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -23,7 +23,6 @@ router.post("/", validateLogin, async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
-    console.log(user);
 
     if (!user) {
         const err = new Error("Login failed");
