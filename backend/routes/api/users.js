b10c436949be95e86166be9480789d8e2960f9f8
cbkinase <cambeck366@gmail.com>
2023-02-22 14:27:09 -0500

    custom error messages for sign-ups and sign-ins

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -10,7 +10,7 @@ const validateSignup = [
     check("email")
         .exists({ checkFalsy: true })
         .isEmail()
-        .withMessage("Please provide a valid email."),
+        .withMessage("Invalid email."),
     check("username")
         .exists({ checkFalsy: true })
         .isLength({ min: 4 })
@@ -23,6 +23,12 @@ const validateSignup = [
         .exists({ checkFalsy: true })
         .isLength({ min: 6 })
         .withMessage("Password must be 6 characters or more."),
+    check("firstName")
+        .exists({ checkFalsy: true })
+        .withMessage("First Name is required"),
+    check("lastName")
+        .exists({ checkFalsy: true })
+        .withMessage("Last Name is required"),
     handleValidationErrors,
 ];
 
@@ -30,10 +36,18 @@ const router = express.Router();
 
 // Sign up
 router.post("/", validateSignup, async (req, res) => {
-    const { email, password, username } = req.body;
-    const user = await User.signup({ email, username, password });
+    const { email, password, firstName, lastName, username } = req.body;
+    let user = await User.signup({
+        email,
+        password,
+        firstName,
+        lastName,
+        username,
+    });
 
-    await setTokenCookie(res, user);
+    const tok = await setTokenCookie(res, user);
+    user = user.toJSON();
+    user.token = tok;
 
     return res.json({
         user: user,
