8f00c85640e6f8c1dd690b2f2a7c346ae1177fb6
cbkinase <cambeck366@gmail.com>
2023-02-25 14:21:04 -0500

    fixed sign up errors

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,6 +6,25 @@ const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
 // Middleware to validate signup
+// async function checkDuplicateUsername(req, res, next) {
+//     console.log(tstDuplicateUsrname);
+
+//     if (tstDuplicateUsrname) {
+//         let err = new Error("User with that username already exists");
+//         err.status = 403;
+//         return next(err);
+//     }
+//     next();
+// }
+// async function checkDuplicateEmail(req, res, next) {
+//     // console.log(tstDuplicateEmail);
+//     if (tstDuplicateEmail) {
+//         let err = new Error("User with that email already exists");
+//         err.status = 403;
+//         return next(err);
+//     }
+//     next();
+// }
 const validateSignup = [
     check("email")
         .exists({ checkFalsy: true })
@@ -37,6 +56,28 @@ const router = express.Router();
 // Sign up
 router.post("/", validateSignup, async (req, res, next) => {
     const { email, password, firstName, lastName, username } = req.body;
+    const checkDuplicateUsername = await User.findOne({
+        where: {
+            username: username,
+        },
+    });
+    const checkDuplicateEmail = await User.findOne({
+        where: {
+            email: email,
+        },
+    });
+
+    if (checkDuplicateEmail || checkDuplicateUsername) {
+        const err = new Error("Error: User Already Exists");
+        err.errors = {};
+        err.status = 403;
+        if (checkDuplicateEmail)
+            err.errors.email = "User with that email already exists";
+        if (checkDuplicateUsername)
+            err.errors.username = "User with that username already exists";
+        throw err;
+    }
+
     let user;
     try {
         user = await User.signup({
