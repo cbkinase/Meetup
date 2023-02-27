0d3b611d1ca12f2f8a3582326d884ccc96f4123a
cbkinase <cambeck366@gmail.com>
2023-02-26 19:36:22 -0500

    Delete an Image for an Event

--- backend/routes/api/event-images.js
+++ backend/routes/api/event-images.js
@@ -18,4 +18,36 @@ const { route } = require("./users");
 
 const router = express.Router();
 
+// Delete an Image for an Event
+
+router.delete("/:imageId", requireAuth, async (req, res, next) => {
+    let eventImage = await EventImage.findByPk(req.params.imageId);
+
+    if (!eventImage) {
+        let err = new Error("Event Image couldn't be found");
+        err.status = 404;
+        return next(err);
+    }
+
+    let event = await eventImage.getEvent();
+    let group = await event.getGroup();
+    let membership = await group.getMemberships({
+        where: {
+            groupId: group.id,
+            userId: req.user.id,
+            status: "co-host",
+        },
+    });
+    if (!membership.length) {
+        let err = new Error("Forbidden");
+        err.status = 403;
+        return next(err);
+    }
+    await eventImage.destroy();
+    return res.json({
+        message: "Successfully deleted",
+        statusCode: 200,
+    });
+});
+
 module.exports = router;
