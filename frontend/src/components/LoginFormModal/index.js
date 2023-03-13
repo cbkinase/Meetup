1c3b6b007f4306bd01be94b6343829ba2673c770
cbkinase <cambeck366@gmail.com>
2023-03-13 12:42:08 -0400

    some css, formatting, validations for login form modal

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
@@ -10,6 +10,25 @@ function LoginFormModal() {
     const [password, setPassword] = useState("");
     const [errors, setErrors] = useState([]);
     const { closeModal } = useModal();
+    const [validationErrors, setValidationErrors] = useState({});
+    const [submitDisabled, setSubmitDisabled] = useState(true);
+
+    useEffect(() => {
+        const errors = {};
+        if (credential.length < 4) {
+            errors.credential =
+                "Username or Email must be at least 4 characters";
+        }
+        if (password.length < 6) {
+            errors.password = "Password must be at least 6 characters";
+        }
+
+        setValidationErrors(errors);
+
+        if (Object.keys(errors).length === 0) {
+            setSubmitDisabled(false);
+        } else setSubmitDisabled(true);
+    }, [credential, password]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -22,36 +41,69 @@ function LoginFormModal() {
             });
     };
 
+    const handleDemoLogin = (e) => {
+        e.preventDefault();
+        const demoUser = {
+            credential: "Demo-lition",
+            password: "password",
+        };
+        return dispatch(sessionActions.login(demoUser))
+            .then(closeModal)
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(Object.values(data.errors));
+            });
+    };
+
     return (
-        <>
-            <h1>Log In</h1>
-            <form onSubmit={handleSubmit}>
+        <div className="login-container">
+            <h1 id="title">Log In</h1>
+            <form className="login-form" onSubmit={handleSubmit}>
                 <ul>
                     {errors.map((error, idx) => (
-                        <li key={idx}>{error}</li>
+                        <li className="errors" key={idx}>
+                            {error}
+                        </li>
                     ))}
                 </ul>
-                <label>
-                    Username or Email
-                    <input
-                        type="text"
-                        value={credential}
-                        onChange={(e) => setCredential(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Password
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </label>
-                <button type="submit">Log In</button>
+                <div className="form-item">
+                    <label>
+                        <input
+                            className="input-field"
+                            type="text"
+                            value={credential}
+                            placeholder="Username or Email"
+                            onChange={(e) => setCredential(e.target.value)}
+                            required
+                        />
+                    </label>
+                    {validationErrors.credential && (
+                        <p className="errors">*{validationErrors.credential}</p>
+                    )}
+                </div>
+                <div className="form-item">
+                    <label>
+                        <input
+                            className="input-field"
+                            placeholder="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </label>
+                    {validationErrors.password && (
+                        <p className="errors">*{validationErrors.password}</p>
+                    )}
+                </div>
+                <button disabled={submitDisabled} id="submit" type="submit">
+                    Log In
+                </button>
+                <button onClick={handleDemoLogin} id="demo">
+                    Demo User
+                </button>
             </form>
-        </>
+        </div>
     );
 }
 
