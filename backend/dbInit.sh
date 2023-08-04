837d5ea308e4072f79aff1fd02224b49507b4dd9
cbkinase <cambeck366@gmail.com>
2023-08-04 17:30:55 -0400

    Change to Docker DB creation/seeding

--- backend/dbInit.sh
+++ backend/dbInit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Check if DB_FILE does not exist, then run migrations and seed
-if [ ! -f "$DB_FILE" ]; then
+# Check if DB_FILE does not exist or is empty
+# If it is then run migrations and seed
+if [ ! -f "$DB_FILE" ] || [ ! -s "$DB_FILE" ]; then
   npx dotenv sequelize-cli db:migrate
   npx dotenv sequelize-cli db:seed:all
 fi
